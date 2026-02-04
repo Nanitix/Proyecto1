9e6663d4043ca29862527e08a3f2ed83d70a75aa
Nanitix <keidysantillan2008@gmail.com>
2026-02-03 21:15:29 -0500

    Add files via upload

---
+++ java/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.librosdigitalesabp;
+
+public class Main {
+    public static void main(String[] args) {
+        new MenuPrincipal().setVisible(true);
+    }
+}
+
